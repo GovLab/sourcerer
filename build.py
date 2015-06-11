d485de80bb153c8cda8de494e4139b050463cf14
Arnaud Sahuguet <arnaud.sahuguet@gmail.com>
2015-06-11 14:46:11 -0400

    changes for Dinorah

--- build.py
+++ build.py
@@ -22,7 +22,7 @@ def Main():
 	env = Environment(loader=FileSystemLoader(TEMPLATES_DIR),
 		extensions=['jinja2.ext.with_'], trim_blocks=True, lstrip_blocks=True)
 	#------------Main pages
-	pages = ["index", 'advisor', 'about', 'form']
+	pages = ['index', 'advisor', 'about', 'form', 'kb']
 	for page in pages:
 		template = env.get_template('%s.html' % page)
 		html = template.render(template_data)
